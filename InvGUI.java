5049da79fdfabfa8cbcb43f2c34526f736a02b55
BaGuAr <92256065+BaGuAr@users.noreply.github.com>
2022-01-16 15:30:42 +0900

    Update Add a few functions

--- InvGUI.java
+++ InvGUI.java
@@ -1,4 +1,4 @@
-package co.n0rthyankt0n.datagui;
+package top.n0rthmaster123.shadeac.gui;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -6,6 +6,7 @@ import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class InvGUI {
@@ -57,5 +58,35 @@ public class InvGUI {
         stack.setItemMeta( meta );
         addItem(stack , index);
         return this;
+    }
+    public InvGUI addItem(ItemStack stack,String name,int amount,int index){
+        ItemStack newStack = stack.clone();
+        newStack.setAmount( amount );
+        ItemMeta meta = newStack.getItemMeta();
+        meta.setDisplayName(name);
+        newStack.setItemMeta(meta);
+        this.inv.setItem(index,newStack);
+        return this;
+    }
+
+    public InvGUI addItem(ItemStack stack,String name,int amount,List list,int index){
+        ItemStack newStack = stack.clone();
+        newStack.setAmount( amount );
+        ItemMeta meta = newStack.getItemMeta();
+        meta.setDisplayName(name);
+        meta.setLore(list);
+        newStack.setItemMeta(meta);
+        this.inv.setItem(index,newStack);
+        return this;
+    }
+
+    @Deprecated
+    public List getAsList(String str){
+        List list = new ArrayList();
+        String[] strx = str.split( "\n" );
+        for( String string : strx ){
+            list.add( string );
+        }
+        return list;
     }
 }
